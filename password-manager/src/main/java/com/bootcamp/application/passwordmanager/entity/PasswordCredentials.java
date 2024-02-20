e97100bfcda109c7669a99b34a075f12e0453b96
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-02-20 06:37:18 +0300

    updates

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/entity/PasswordCredentials.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/entity/PasswordCredentials.java
@@ -1,14 +1,11 @@
 package com.bootcamp.application.passwordmanager.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
-
+@Table()
 public class PasswordCredentials {
 
     @Id
