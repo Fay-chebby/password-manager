d7e2274f28a1b58c38588142e5fcd822026b2703
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-24 20:42:00 +0300

    ...

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/models/Password.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/models/Password.java
@@ -1,6 +1,8 @@
 package com.bootcamp.application.passwordmanager.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,6 +10,8 @@ import lombok.Data;
 @Entity
 public class Password {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String password;
     private String website;
 }
--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/repositories/ManagedPasswordsRepository.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/repositories/ManagedPasswordsRepository.java
@@ -1,4 +1,9 @@
 package com.bootcamp.application.passwordmanager.repositories;
 
-public interface ManagedPasswordsRepository {
+import com.bootcamp.application.passwordmanager.models.Password;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManagedPasswordsRepository extends JpaRepository<Password,Long> {
 }
