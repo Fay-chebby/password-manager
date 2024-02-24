5903bfbfbf70b2f4658e39463b180d8d276b808e
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-24 20:39:38 +0300

    ...

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/models/Password.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/models/Password.java
@@ -1,9 +1,13 @@
 package com.bootcamp.application.passwordmanager.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Password {
+    @Id
     private String password;
     private String website;
 }
