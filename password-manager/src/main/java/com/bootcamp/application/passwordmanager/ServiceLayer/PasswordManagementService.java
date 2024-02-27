720da73a4e53a55bc6a4e2ddce4206d01b93977f
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-27 10:42:55 +0300

    encryption

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/ServiceLayer/PasswordManagementService.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/ServiceLayer/PasswordManagementService.java
@@ -5,6 +5,7 @@ import com.bootcamp.application.passwordmanager.configurations.encrption.Passwor
 import com.bootcamp.application.passwordmanager.models.Password;
 import com.bootcamp.application.passwordmanager.repositories.ManagedPasswordsRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.BadPaddingException;
@@ -16,14 +17,16 @@ import java.security.NoSuchAlgorithmException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PasswordManagementService {
     private final ManagedPasswordsRepository managedPasswordsRepository;
     private final PasswordDetailsEncryption passwordDetailsEncryption;
-    private final String SEC_KEY = "@builtByMiroweBob";
-    private final String IV = "@BobmiroweIvKey";
+    private final String SEC_KEY = "";
+    private final String IV = "";
 
     public Password encryptDetails(PasswordFront passwordFront) throws InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
         Password password = new Password();
+        log.info("service to encode is reached");
         passwordDetailsEncryption.exportStrings(SEC_KEY,IV);
         password.setPassword(passwordDetailsEncryption.encryptingMethod(
                 passwordFront.getPassword()
@@ -31,14 +34,15 @@ public class PasswordManagementService {
         password.setWebsite(passwordDetailsEncryption.encryptingMethod(
                 passwordFront.getWebsite()
         ));
+        log.info("method was success");
         return managedPasswordsRepository.save(password);
     }
-    public Password decryptDetails(String username){
+    /*public Password decryptDetails(String username){
         try {
             Password details = managedPasswordsRepository.findAllByUsername(username);
             return details;
         }catch (Exception e){
             return null;
         }
-    }
+    }*/
 }
--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/configurations/encrption/PasswordDetailsEncryption.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/configurations/encrption/PasswordDetailsEncryption.java
@@ -1,5 +1,7 @@
 package com.bootcamp.application.passwordmanager.configurations.encrption;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.*;
@@ -9,13 +11,15 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
-@Component
+@Configuration
+@Slf4j
 public class PasswordDetailsEncryption {
 
     private SecretKey SECRET_KEY;
     private int KEY_SIZE = 128;
     private byte[] IV;
     private final int tlen = 128;
+
 
 
     public void init() throws NoSuchAlgorithmException {
@@ -32,6 +36,7 @@ public class PasswordDetailsEncryption {
             InvalidKeyException,
             IllegalBlockSizeException,
             BadPaddingException {
+        log.info("this method to encrypt was called");
         byte[] messageToEncrypt = message.getBytes();
 
         Cipher encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
@@ -39,6 +44,7 @@ public class PasswordDetailsEncryption {
         encryptionCipher.init(Cipher.ENCRYPT_MODE,SECRET_KEY,spec);
 
         byte[] encodedByteMessage = encryptionCipher.doFinal(messageToEncrypt);
+        log.info("the method ended successfully");
         return encode(encodedByteMessage);
 
     }
@@ -50,21 +56,27 @@ public class PasswordDetailsEncryption {
             InvalidKeyException,
             IllegalBlockSizeException,
             BadPaddingException {
+        log.info("decoding method was called");
         byte[] messageToDecrypt = decode(encodedMessage);
         Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
         GCMParameterSpec spec = new GCMParameterSpec(tlen,IV);
         decryptionCipher.init(Cipher.DECRYPT_MODE,SECRET_KEY,spec);
         byte[] decryptedMessage = decryptionCipher.doFinal(messageToDecrypt);
+        log.info("the method ended successfully");
         return new String(decryptedMessage);
     }
     public void exportStrings(String secretKey,String IV){
+        log.info("method to export the keys is reached");
         SECRET_KEY = new SecretKeySpec(decode(secretKey),"AES");
         this.IV = decode(IV);
+        log.info("export success");
     }
     public String encode(byte[] data){
+        log.info("encoding to occur");
         return Base64.getEncoder().encodeToString(data);
     }
     public byte[] decode(String data){
+        log.info("decoding to begin");
         return Base64.getDecoder().decode(data);
     }
 }
