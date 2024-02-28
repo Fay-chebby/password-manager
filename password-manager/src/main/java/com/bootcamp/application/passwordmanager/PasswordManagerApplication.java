5418712fa0811c15ba8e0a838afdcb5620f9efc1
Fai-chebby <chebetf034@gmail.com>
2024-02-28 15:22:41 +0300

    Password

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/PasswordManagerApplication.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/PasswordManagerApplication.java
@@ -2,12 +2,15 @@ package com.bootcamp.application.passwordmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@CrossOrigin
 public class PasswordManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordManagerApplication.class, args);
 	}
-
 }
