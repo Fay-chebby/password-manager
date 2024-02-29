337aa46851a1f8e19ac2f5c91fb8c804c14fcc7f
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-29 13:56:39 +0300

    ...

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/Configurations/CryptoUtils.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/Configurations/CryptoUtils.java
@@ -1,4 +1,22 @@
 package com.bootcamp.application.passwordmanager.Configurations;
 
+import org.springframework.stereotype.Component;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+@Component
 public class CryptoUtils {
+
+    //generate the secret key from random number
+    public static SecretKey generateSecretKey() throws Exception{
+        int size = 256;
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(size);
+        SecretKey SECRET_KEY = generator.generateKey();
+        return SECRET_KEY;
+    }
+
+    //
+
 }
