acb0ad8d5cf738f372039154a2213f859b60da9e
Bobcodiz <mirowe.okinyi22@students.dkut.ac.ke>
2024-02-29 14:20:38 +0300

    ...

--- password-manager/src/main/java/com/bootcamp/application/passwordmanager/Configurations/CryptoObjectENCDECUtil.java
+++ password-manager/src/main/java/com/bootcamp/application/passwordmanager/Configurations/CryptoObjectENCDECUtil.java
@@ -1,12 +1,14 @@
 package com.bootcamp.application.passwordmanager.Configurations;
 
+import org.springframework.stereotype.Component;
+
 import javax.crypto.Cipher;
 import javax.crypto.SealedObject;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import java.io.Serializable;
-
-public class CryptoObjectEncryptionUtil {
+@Component
+public class CryptoObjectENCDECUtil {
 
     public static SealedObject encryptObject
             (String algorithm, Serializable object,
@@ -16,5 +18,14 @@ public class CryptoObjectEncryptionUtil {
         encryptionCipher.init(Cipher.ENCRYPT_MODE,key,iv);
         SealedObject sealedObject = new SealedObject(object,encryptionCipher);
         return sealedObject;
+    }
+
+    public static Serializable decryptObject
+            (String algorithm, SealedObject object,
+             SecretKey key, IvParameterSpec iv)throws Exception{
+        Cipher decryptionCipher = Cipher.getInstance(algorithm);
+        decryptionCipher.init(Cipher.DECRYPT_MODE,key,iv);
+        Serializable unsealedObject = (Serializable) object.getObject(decryptionCipher);
+        return unsealedObject;
     }
 }
